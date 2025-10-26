35878165ac51eee3b4b37fed9c1745bab9c9a3be
Karesis <yangyifeng23@mails.ucas.ac.cn>
2025-10-26 19:36:34 +0800

    fix(ir/inst): use as union to reach pointee_type

--- src/ir/instruction.c
+++ src/ir/instruction.c
@@ -186,7 +186,7 @@ ir_instruction_dump(IRInstruction *inst, FILE *stream)
   case IR_OP_ALLOCA:
     fprintf(stream, "alloca ");
     assert(inst->result.type->kind == IR_TYPE_PTR);
-    ir_type_dump(inst->result.type->pointee_type, stream);
+    ir_type_dump(inst->result.type->as.pointee_type, stream);
     break;
 
   case IR_OP_LOAD:
