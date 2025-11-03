87f3ba700f90da98911b1f41370bbb23c46bb28c
Karesis <yangyifeng23@mails.ucas.ac.cn>
2025-11-03 14:49:18 +0800

    feat(ir/instr): add more instrs

--- include/ir/instruction.h
+++ include/ir/instruction.h
@@ -24,36 +24,113 @@
 
 typedef enum
 {
-
+  // --- 终止指令 (Terminators) ---
   IR_OP_RET,
   IR_OP_BR,
   IR_OP_COND_BR,
+  IR_OP_SWITCH,
 
+  // --- 整数二元运算 ---
   IR_OP_ADD,
   IR_OP_SUB,
-  IR_OP_ICMP,
+  IR_OP_MUL,
+  IR_OP_UDIV,
+  IR_OP_SDIV,
+  IR_OP_UREM,
+  IR_OP_SREM,
 
+  // --- 浮点二元运算 ---
+  IR_OP_FADD,
+  IR_OP_FSUB,
+  IR_OP_FMUL,
+  IR_OP_FDIV,
+
+  // --- 位运算 ---
+  IR_OP_SHL,
+  IR_OP_LSHR, /// 逻辑右移
+  IR_OP_ASHR, /// 算术右移
+  IR_OP_AND,
+  IR_OP_OR,
+  IR_OP_XOR,
+
+  // --- 内存操作 ---
   IR_OP_ALLOCA,
   IR_OP_LOAD,
   IR_OP_STORE,
-  IR_OP_PHI,
   IR_OP_GEP,
+
+  // --- 比较操作 ---
+  IR_OP_ICMP,
+  IR_OP_FCMP, // 浮点比较
+
+  // --- 类型转换 (Casting) ---
+  IR_OP_TRUNC,    /// 截断 (i64 -> i32)
+  IR_OP_ZEXT,     /// 零扩展 (i32 -> i64)
+  IR_OP_SEXT,     /// 符号扩展 (i32 -> i64)
+  IR_OP_FPTRUNC,  /// 浮点截断 (f64 -> f32)
+  IR_OP_FPEXT,    /// 浮点扩展 (f32 -> f64)
+  IR_OP_FPTOUI,   /// 浮点 -> 无符号整数
+  IR_OP_FPTOSI,   /// 浮点 -> 有符号整数
+  IR_OP_UITOFP,   /// 无符号整数 -> 浮点
+  IR_OP_SITOFP,   /// 有符号整数 -> 浮点
+  IR_OP_PTRTOINT, /// 指针 -> 整数
+  IR_OP_INTTOPTR, /// 整数 -> 指针
+  IR_OP_BITCAST,  /// 位转换 (e.g., i32 -> f32)
+
+  // --- 其他 ---
+  IR_OP_PHI,
   IR_OP_CALL
+
 } IROpcode;
 
 typedef enum
 {
-  IR_ICMP_EQ,
-  IR_ICMP_NE,
-  IR_ICMP_UGT,
-  IR_ICMP_UGE,
-  IR_ICMP_ULT,
-  IR_ICMP_ULE,
-  IR_ICMP_SGT,
-  IR_ICMP_SGE,
-  IR_ICMP_SLT,
-  IR_ICMP_SLE,
+  IR_ICMP_EQ,  /// 等于
+  IR_ICMP_NE,  /// 不等于
+  IR_ICMP_UGT, /// 无符号大于
+  IR_ICMP_UGE, /// 无符号大于等于
+  IR_ICMP_ULT, /// 无符号小于
+  IR_ICMP_ULE, /// 无符号小于等于
+  IR_ICMP_SGT, /// 有符号大于
+  IR_ICMP_SGE, /// 有符号大于等于
+  IR_ICMP_SLT, /// 有符号小于
+  IR_ICMP_SLE, /// 有符号小于等于
 } IRICmpPredicate;
+
+/**
+ * @brief 浮点数比较 (FCMP) 的谓词
+ * 'O' = Ordered (有序, 操作数非 NaN)
+ * 'U' = Unordered (无序, 操作数至少有一个是 NaN)
+ */
+typedef enum
+{
+  // --- Ordered (O) ---
+  /// (如果任一操作数为 NaN, 结果为 false)
+  IR_FCMP_OEQ, /// Ordered and Equal (有序且等于)
+  IR_FCMP_OGT, /// Ordered and Greater Than (有序且大于)
+  IR_FCMP_OGE, /// Ordered and Greater Than or Equal (有序且大于等于)
+  IR_FCMP_OLT, /// Ordered and Less Than (有序且小于)
+  IR_FCMP_OLE, /// Ordered and Less Than or Equal (有序且小于等于)
+  IR_FCMP_ONE, /// Ordered and Not Equal (有序且不等于)
+
+  // --- Unorde/red (U) ---
+  /// (如果任一/操作数为 NaN, 结果为 true)
+  IR_FCMP_UEQ, /// Unordered or Equal (无序或等于)
+  IR_FCMP_UGT, /// Unordered or Greater Than (无序或大于)
+  IR_FCMP_UGE, /// Unordered or Greater Than or Equal (无序或大于等于)
+  IR_FCMP_ULT, /// Unordered or Less Than (无序或小于)
+  IR_FCMP_ULE, /// Unordered or Less Than or Equal (无序或小于等于)
+  IR_FCMP_UNE, /// Unordered or Not Equal (无序或不等于)
+
+  // --- 特殊谓词 ---
+  IR_FCMP_ORD, /// Ordered (有序, 检查两个操作数都不是 NaN)
+  IR_FCMP_UNO, /// Unordered (无序, 检查至少一个操作数是 NaN)
+
+  /// 始终为 true/false
+  IR_FCMP_TRUE,
+  IR_FCMP_FALSE,
+
+} IRFCmpPredicate;
 
 typedef struct
 {
@@ -70,6 +147,12 @@ typedef struct
       IRICmpPredicate predicate;
     } icmp;
 
+    // 新增
+    struct
+    {
+      IRFCmpPredicate predicate;
+    } fcmp;
+
     struct
     {
       IRType *source_type;
@@ -85,7 +168,6 @@ void ir_instruction_erase_from_parent(IRInstruction *inst);
 
 /**
  * @brief 将单条指令的 IR 打印到 IRPrinter
- * [!!] 签名已更改
  * @param inst 要打印的指令
  * @param p 打印机 (策略)
  */
--- src/ir/instruction.c
+++ src/ir/instruction.c
@@ -59,6 +59,49 @@ ir_icmp_predicate_to_string(IRICmpPredicate pred)
   }
 }
 
+// [!!] 新增的辅助函数
+static const char *
+ir_fcmp_predicate_to_string(IRFCmpPredicate pred)
+{
+  switch (pred)
+  {
+  case IR_FCMP_OEQ:
+    return "oeq";
+  case IR_FCMP_OGT:
+    return "ogt";
+  case IR_FCMP_OGE:
+    return "oge";
+  case IR_FCMP_OLT:
+    return "olt";
+  case IR_FCMP_OLE:
+    return "ole";
+  case IR_FCMP_ONE:
+    return "one";
+  case IR_FCMP_UEQ:
+    return "ueq";
+  case IR_FCMP_UGT:
+    return "ugt";
+  case IR_FCMP_UGE:
+    return "uge";
+  case IR_FCMP_ULT:
+    return "ult";
+  case IR_FCMP_ULE:
+    return "ule";
+  case IR_FCMP_UNE:
+    return "une";
+  case IR_FCMP_ORD:
+    return "ord";
+  case IR_FCMP_UNO:
+    return "uno";
+  case IR_FCMP_TRUE:
+    return "true";
+  case IR_FCMP_FALSE:
+    return "false";
+  default:
+    return "??";
+  }
+}
+
 /**
  * @brief (内部) 获取第 N 个操作数
  */
@@ -112,6 +155,9 @@ ir_instruction_erase_from_parent(IRInstruction *inst)
   list_del(&inst->list_node);
 }
 
+/**
+ * @brief 将单条指令的 IR 打印到 IRPrinter
+ */
 void
 ir_instruction_dump(IRInstruction *inst, IRPrinter *p)
 {
@@ -121,21 +167,23 @@ ir_instruction_dump(IRInstruction *inst, IRPrinter *p)
     return;
   }
 
+  // 1. 打印结果 (e.g., "%res: i32 = ")
   int has_result = (inst->result.type && inst->result.type->kind != IR_TYPE_VOID);
   if (has_result)
   {
-
     ir_value_dump_with_type(&inst->result, p);
     ir_print_str(p, " = ");
   }
 
-  IRValueNode *op1, *op2;
+  IRValueNode *op1, *op2, *op3;
 
+  // 2. 打印指令体 (分发)
   switch (inst->opcode)
   {
+  /// --- 终止指令 ---
   case IR_OP_RET:
     ir_print_str(p, "ret ");
-    op1 = get_operand(inst, 0);
+    op1 = get_operand(inst, 0); // May be NULL for "ret void"
     if (op1)
     {
       ir_value_dump_with_type(op1, p);
@@ -150,98 +198,133 @@ ir_instruction_dump(IRInstruction *inst, IRPrinter *p)
     ir_print_str(p, "br ");
     op1 = get_operand(inst, 0);
     assert(op1 && "br must have a target");
-    ir_value_dump_with_type(op1, p);
+    ir_value_dump_with_type(op1, p); // 打印 "$label"
     break;
 
   case IR_OP_COND_BR:
     ir_print_str(p, "br ");
-    IRValueNode *cond = get_operand(inst, 0);
-    IRValueNode *true_bb = get_operand(inst, 1);
-    IRValueNode *false_bb = get_operand(inst, 2);
-    assert(cond && true_bb && false_bb && "cond br needs 3 operands");
+    op1 = get_operand(inst, 0); /// cond
+    op2 = get_operand(inst, 1); /// true_bb
+    op3 = get_operand(inst, 2); /// false_bb
+    assert(op1 && op2 && op3 && "cond br needs 3 operands");
 
-    ir_value_dump_with_type(cond, p);
+    ir_value_dump_with_type(op1, p); /// %cond: i1
     ir_print_str(p, ", ");
-    ir_value_dump_with_type(true_bb, p);
+    ir_value_dump_with_type(op2, p); /// $true_bb
     ir_print_str(p, ", ");
-    ir_value_dump_with_type(false_bb, p);
+    ir_value_dump_with_type(op3, p); /// $false_bb
     break;
 
+  case IR_OP_SWITCH:
+    ir_print_str(p, "switch ");
+    op1 = get_operand(inst, 0); /// cond
+    op2 = get_operand(inst, 1); /// default_bb
+    assert(op1 && op2 && "switch needs at least cond and default");
+
+    ir_value_dump_with_type(op1, p); /// %cond: i32
+    ir_print_str(p, ", default ");
+    ir_value_dump_with_type(op2, p); /// $default_bb
+
+    ir_print_str(p, " [");
+
+    // 遍历 case, 它们从索引 2 开始，成对出现
+    int i = 2;
+    IRValueNode *case_val = get_operand(inst, i);
+    while (case_val)
+    {
+      IRValueNode *case_bb = get_operand(inst, i + 1);
+      assert(case_bb && "switch case pair is incomplete");
+
+      ir_print_str(p, "\n    ");            // 缩进
+      ir_value_dump_with_type(case_val, p); // 10: i32
+      ir_print_str(p, ", ");
+      ir_value_dump_with_type(case_bb, p); // $case_1
+
+      i += 2;
+      case_val = get_operand(inst, i);
+    }
+    ir_print_str(p, "\n  ]"); // 闭合
+    break;
+
+  /// --- 整数二元运算 ---
   case IR_OP_ADD:
+    ir_dump_binary_op(inst, p, "add");
+    break;
   case IR_OP_SUB:
-    ir_print_str(p, (inst->opcode == IR_OP_ADD) ? "add " : "sub ");
-    op1 = get_operand(inst, 0);
-    op2 = get_operand(inst, 1);
-    assert(op1 && op2 && "Binary operator needs two operands");
-
-    ir_value_dump_with_type(op1, p);
-    ir_print_str(p, ", ");
-    ir_value_dump_with_type(op2, p);
+    ir_dump_binary_op(inst, p, "sub");
+    break;
+  case IR_OP_MUL:
+    ir_dump_binary_op(inst, p, "mul");
+    break;
+  case IR_OP_UDIV:
+    ir_dump_binary_op(inst, p, "udiv");
+    break;
+  case IR_OP_SDIV:
+    ir_dump_binary_op(inst, p, "sdiv");
+    break;
+  case IR_OP_UREM:
+    ir_dump_binary_op(inst, p, "urem");
+    break;
+  case IR_OP_SREM:
+    ir_dump_binary_op(inst, p, "srem");
     break;
 
+  /// --- 浮点二元运算 ---
+  case IR_OP_FADD:
+    ir_dump_binary_op(inst, p, "fadd");
+    break;
+  case IR_OP_FSUB:
+    ir_dump_binary_op(inst, p, "fsub");
+    break;
+  case IR_OP_FMUL:
+    ir_dump_binary_op(inst, p, "fmul");
+    break;
+  case IR_OP_FDIV:
+    ir_dump_binary_op(inst, p, "fdiv");
+    break;
+
+  /// --- 位运算 ---
+  case IR_OP_SHL:
+    ir_dump_binary_op(inst, p, "shl");
+    break;
+  case IR_OP_LSHR:
+    ir_dump_binary_op(inst, p, "lshr");
+    break;
+  case IR_OP_ASHR:
+    ir_dump_binary_op(inst, p, "ashr");
+    break;
+  case IR_OP_AND:
+    ir_dump_binary_op(inst, p, "and");
+    break;
+  case IR_OP_OR:
+    ir_dump_binary_op(inst, p, "or");
+    break;
+  case IR_OP_XOR:
+    ir_dump_binary_op(inst, p, "xor");
+    break;
+
+  /// --- 内存操作 ---
   case IR_OP_ALLOCA:
     ir_print_str(p, "alloc ");
     assert(inst->result.type->kind == IR_TYPE_PTR);
-
-    ir_type_dump(inst->result.type->as.pointee_type, p);
+    ir_type_dump(inst->result.type->as.pointee_type, p); // 打印被分配的类型
     break;
 
   case IR_OP_LOAD:
     ir_print_str(p, "load ");
     op1 = get_operand(inst, 0);
     assert(op1 && "load needs a pointer operand");
-    ir_value_dump_with_type(op1, p);
+    ir_value_dump_with_type(op1, p); // %ptr: <*i32>
     break;
 
   case IR_OP_STORE:
     ir_print_str(p, "store ");
-    op1 = get_operand(inst, 0);
-    op2 = get_operand(inst, 1);
+    op1 = get_operand(inst, 0); // value
+    op2 = get_operand(inst, 1); // pointer
     assert(op1 && op2 && "store needs value and pointer operands");
-
-    ir_value_dump_with_type(op1, p);
+    ir_value_dump_with_type(op1, p); // %val: i32
     ir_print_str(p, ", ");
-    ir_value_dump_with_type(op2, p);
-    break;
-
-  case IR_OP_ICMP:
-    op1 = get_operand(inst, 0);
-    op2 = get_operand(inst, 1);
-    assert(op1 && op2 && "icmp needs two operands");
-
-    ir_printf(p, "icmp %s ", ir_icmp_predicate_to_string(inst->as.icmp.predicate));
-    ir_value_dump_with_type(op1, p);
-    ir_print_str(p, ", ");
-    ir_value_dump_with_type(op2, p);
-    break;
-
-  case IR_OP_PHI:
-    ir_print_str(p, "phi ");
-    IDList *head = &inst->operands;
-    IDList *iter = head->next;
-    int i = 0;
-    while (iter != head)
-    {
-      if (i > 0)
-      {
-        ir_print_str(p, ", ");
-      }
-      IRUse *val_use = list_entry(iter, IRUse, user_node);
-      IRValueNode *val = val_use->value;
-      iter = iter->next;
-      assert(iter != head && "PHI node must have [val, bb] pairs");
-      IRUse *bb_use = list_entry(iter, IRUse, user_node);
-      IRValueNode *bb = bb_use->value;
-
-      ir_print_str(p, "[ ");
-      ir_value_dump_with_type(val, p);
-      ir_print_str(p, ", ");
-      ir_value_dump_with_type(bb, p);
-      ir_print_str(p, " ]");
-
-      iter = iter->next;
-      i++;
-    }
+    ir_value_dump_with_type(op2, p); // %ptr: <*i32>
     break;
 
   case IR_OP_GEP:
@@ -251,52 +334,172 @@ ir_instruction_dump(IRInstruction *inst, IRPrinter *p)
       ir_print_str(p, "inbounds ");
     }
 
-    IDList *ghead = &inst->operands;
-    IDList *giter = ghead->next;
-    while (giter != ghead)
-    {
-      IRUse *use = list_entry(giter, IRUse, user_node);
-      ir_value_dump_with_type(use->value, p);
+    op1 = get_operand(inst, 0); // base pointer
+    assert(op1 && "gep must have at least one operand");
+    ir_value_dump_with_type(op1, p);
 
-      giter = giter->next;
-      if (giter != ghead)
+    // 打印索引
+    int gep_idx = 1;
+    IRValueNode *idx = get_operand(inst, gep_idx);
+    while (idx)
+    {
+      ir_print_str(p, ", ");
+      ir_value_dump_with_type(idx, p);
+      idx = get_operand(inst, ++gep_idx);
+    }
+    break;
+
+  /// --- 比较操作 ---
+  case IR_OP_ICMP:
+    op1 = get_operand(inst, 0);
+    op2 = get_operand(inst, 1);
+    assert(op1 && op2 && "icmp needs two operands");
+    ir_printf(p, "icmp %s ", ir_icmp_predicate_to_string(inst->as.icmp.predicate));
+    ir_value_dump_with_type(op1, p);
+    ir_print_str(p, ", ");
+    ir_value_dump_with_type(op2, p);
+    break;
+
+  case IR_OP_FCMP:
+    op1 = get_operand(inst, 0);
+    op2 = get_operand(inst, 1);
+    assert(op1 && op2 && "fcmp needs two operands");
+    ir_printf(p, "fcmp %s ", ir_fcmp_predicate_to_string(inst->as.fcmp.predicate));
+    ir_value_dump_with_type(op1, p);
+    ir_print_str(p, ", ");
+    ir_value_dump_with_type(op2, p);
+    break;
+
+  /// --- 类型转换 (Casting) ---
+  case IR_OP_TRUNC:
+    ir_dump_cast_op(inst, p, "trunc");
+    break;
+  case IR_OP_ZEXT:
+    ir_dump_cast_op(inst, p, "zext");
+    break;
+  case IR_OP_SEXT:
+    ir_dump_cast_op(inst, p, "sext");
+    break;
+  case IR_OP_FPTRUNC:
+    ir_dump_cast_op(inst, p, "fptrunc");
+    break;
+  case IR_OP_FPEXT:
+    ir_dump_cast_op(inst, p, "fpext");
+    break;
+  case IR_OP_FPTOUI:
+    ir_dump_cast_op(inst, p, "fptoui");
+    break;
+  case IR_OP_FPTOSI:
+    ir_dump_cast_op(inst, p, "fptosi");
+    break;
+  case IR_OP_UITOFP:
+    ir_dump_cast_op(inst, p, "uitofp");
+    break;
+  case IR_OP_SITOFP:
+    ir_dump_cast_op(inst, p, "sitofp");
+    break;
+  case IR_OP_PTRTOINT:
+    ir_dump_cast_op(inst, p, "ptrtoint");
+    break;
+  case IR_OP_INTTOPTR:
+    ir_dump_cast_op(inst, p, "inttoptr");
+    break;
+  case IR_OP_BITCAST:
+    ir_dump_cast_op(inst, p, "bitcast");
+    break;
+
+  /// --- 其他 ---
+  case IR_OP_PHI:
+    ir_print_str(p, "phi ");
+    int phi_i = 0;
+    IRValueNode *phi_val = get_operand(inst, phi_i);
+    while (phi_val)
+    {
+      IRValueNode *phi_bb = get_operand(inst, phi_i + 1);
+      assert(phi_bb && "PHI node must have [val, bb] pairs");
+
+      if (phi_i > 0)
       {
         ir_print_str(p, ", ");
       }
+      ir_print_str(p, "[ ");
+      ir_value_dump_with_type(phi_val, p);
+      ir_print_str(p, ", ");
+      ir_value_dump_with_type(phi_bb, p);
+      ir_print_str(p, " ]");
+
+      phi_i += 2;
+      phi_val = get_operand(inst, phi_i);
     }
     break;
 
   case IR_OP_CALL: {
     ir_print_str(p, "call ");
-    IRValueNode *callee = get_operand(inst, 0);
-    assert(callee != NULL && "call must have a callee");
+    op1 = get_operand(inst, 0); // callee
+    assert(op1 != NULL && "call must have a callee");
 
-    ir_type_dump(callee->type, p);
+    // 打印函数签名 (callee 的类型)
+    assert(op1->type->kind == IR_TYPE_PTR);
+    assert(op1->type->as.pointee_type->kind == IR_TYPE_FUNCTION);
+    ir_type_dump(op1->type->as.pointee_type, p);
+
     ir_print_str(p, " ");
-    ir_value_dump_name(callee, p);
+    ir_value_dump_name(op1, p); // @func_name
     ir_print_str(p, "(");
 
-    IDList *chead = &inst->operands;
-    IDList *citer = chead->next->next;
-    int arg_idx = 0;
-    while (citer != chead)
+    // 打印参数 (从索引 1 开始)
+    int arg_idx = 1;
+    IRValueNode *arg = get_operand(inst, arg_idx);
+    while (arg)
     {
-      if (arg_idx > 0)
+      if (arg_idx > 1)
       {
         ir_print_str(p, ", ");
       }
-      IRUse *use = list_entry(citer, IRUse, user_node);
-      ir_value_dump_with_type(use->value, p);
-
-      citer = citer->next;
-      arg_idx++;
+      ir_value_dump_with_type(arg, p);
+      arg = get_operand(inst, ++arg_idx);
     }
     ir_print_str(p, ")");
     break;
   }
 
   default:
-    ir_printf(p, "<?? opcode %d>", inst->opcode);
+    ir_printf(p, "<?? UNIMPLEMENTED OPCODE %d>", inst->opcode);
     break;
   }
+}
+
+/**
+ * @brief (内部) 打印二元运算 (e.g., "add %a: i32, %b: i32")
+ */
+static void
+ir_dump_binary_op(IRInstruction *inst, IRPrinter *p, const char *opname)
+{
+  IRValueNode *op1 = get_operand(inst, 0);
+  IRValueNode *op2 = get_operand(inst, 1);
+  assert(op1 && op2 && "Binary operator needs two operands");
+
+  ir_print_str(p, opname);
+  ir_print_str(p, " ");
+  ir_value_dump_with_type(op1, p);
+  ir_print_str(p, ", ");
+  ir_value_dump_with_type(op2, p);
+}
+
+/**
+ * @brief (内部) 打印类型转换 (e.g., "zext %a: i32 to i64")
+ */
+static void
+ir_dump_cast_op(IRInstruction *inst, IRPrinter *p, const char *opname)
+{
+  IRValueNode *op1 = get_operand(inst, 0);
+  assert(op1 && "Cast operator needs an operand");
+
+  ir_print_str(p, opname);
+  ir_print_str(p, " ");
+  ir_value_dump_with_type(op1, p);
+
+  /// 语法: "zext %op: <type> to <result_type>"
+  ir_print_str(p, " to ");
+  ir_type_dump(inst->result.type, p);
 }
